11b4363e389603d9dc706f7e08d242866d240cd3
Henry <hgc509@york.ac.uk>
2016-06-24 17:33:53 +0100

    Set up slick

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 /**
  * Created by Henry on 24/06/2016.
  */
+import org.newdawn.slick.*;
+
+
 public class Main {
 
     public static void main(String args[]){
-        System.out.println("Welcome to Matt and Henry Hacking out a game");
+        System.out.println("LWJGL version: " + "Welcome to Matt and Henry Hacking out a game");
 
     }
 }
