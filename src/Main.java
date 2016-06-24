2336d42fa8705113b36edb8e06c1686a8b95b64c
Henry <hgc509@york.ac.uk>
2016-06-24 16:39:59 +0100

    Initial setup

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Henry on 24/06/2016.
+ */
+public class Main {
+
+    public static void main(String args[]){
+        System.out.println("Welcome to Matt and Henry Hacking out a game");
+
+    }
+}
