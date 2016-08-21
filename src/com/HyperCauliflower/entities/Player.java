48d71c35cfcc7097656ef4f67a9f0953029f333b
Tim020 <timbradgate@hotmail.co.uk>
2016-08-21 23:11:16 +0100

    Particles no longer stop immediately after the player stops walking

--- src/com/HyperCauliflower/entities/Player.java
+++ src/com/HyperCauliflower/entities/Player.java
@@ -9,6 +9,7 @@ import org.newdawn.slick.particles.ConfigurableEmitter;
 import org.newdawn.slick.particles.ParticleIO;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 
 /**
  * Created by Matt on 24/06/2016.
@@ -16,7 +17,7 @@ import java.io.IOException;
 public class Player extends Entity {
 
     //load in all values from json to avoid further reads thus being more efficient
-    private Point mousePos = new Point(0,0);
+    private Point mousePos = new Point(0, 0);
     private Point playerAbsPos;
     public ConfigurableEmitter footsteps;
     private Inventory inventory;
@@ -29,8 +30,8 @@ public class Player extends Entity {
 
         try {
             footsteps = ParticleIO.loadEmitter("/res/sprites/Particles/footsteps.xml");
-
-        }catch(IOException e){
+            footsteps.setEnabled(true);
+        } catch (IOException e) {
             e.printStackTrace();
             System.exit(3);
         }
@@ -44,16 +45,16 @@ public class Player extends Entity {
 
         double direction = 0;
         if (dir == 0) {
-            direction=facing;
+            direction = facing;
         }
         if (dir == 1) {
-            direction=(facing+Math.PI/2);
+            direction = (facing + Math.PI / 2);
         }
         if (dir == 2) {
-            direction = (facing+Math.PI);
+            direction = (facing + Math.PI);
         }
-        if(dir == 3) {
-            direction = (facing-Math.PI/2);
+        if (dir == 3) {
+            direction = (facing - Math.PI / 2);
         }
 
         moveVector = moveVector.translate(new Point(Math.cos(direction), Math.sin(direction)));
@@ -62,14 +63,14 @@ public class Player extends Entity {
     public void update(GameState game) {
         super.update(game);
         mousePos = game.getMousePosition();
-        footsteps.update(game.pSystem,game.getDelta());
+        footsteps.update(game.pSystem, game.getDelta());
 
     }
 
     public void render(Graphics graphics, Point offset) {
         rotatePlayer(offset);
         super.render(graphics, offset);
-        footsteps.setPosition(this.getLocation().getX(), this.getLocation().getY(),true);
+        footsteps.setPosition(this.getLocation().getX(), this.getLocation().getY(), true);
     }
 
     public int getAnimationFrame() {
@@ -81,21 +82,21 @@ public class Player extends Entity {
         facing = Math.atan2(this.mousePos.getY() - (this.getLocation().getY() + p.getY()), this.mousePos.getX() - ((this.getLocation().getX() + p.getX())));
     }
 
-    public void enableFootsteps(){
-
-        footsteps.setEnabled(true);
-    }
-    public void disableFootsteps(){
-        footsteps.resetState();
-        footsteps.setEnabled(false);
+    public void enableFootsteps() {
+        footsteps.spawnCount.setMax(5);
+        footsteps.spawnCount.setMin(5);
     }
 
+    public void disableFootsteps() {
+        footsteps.spawnCount.setMax(0);
+        footsteps.spawnCount.setMin(0);
+    }
 
-    public void usePrimary(){
+    public void usePrimary() {
         if (this.inventory.getEquipped() != null) {
             this.inventory.getEquipped().fire(mousePos);
 
-        } else{
+        } else {
             //print no primary weapon equipped
         }
     }
