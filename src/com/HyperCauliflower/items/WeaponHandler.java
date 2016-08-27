d209d5bbff7283a320f1441ca6c0af67d62f1626
Matt <matt.barnfield4@gmail.com>
2016-08-27 22:41:15 +0100

    json

--- src/com/HyperCauliflower/items/WeaponHandler.java
+++ src/com/HyperCauliflower/items/WeaponHandler.java
@@ -6,6 +6,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.lwjgl.Sys;
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -21,6 +22,7 @@ public class WeaponHandler {
     public WeaponHandler(SpriteSheetData projectileSprites){
         try {
             weapons = new HashMap<>();
+            projectiles = new HashMap<>();
             JSONObject obj = (JSONObject)new JSONParser().parse(new FileReader(PATH));
             for(Object weapon:(JSONArray)obj.get("weapons")){
                 JSONObject w = (JSONObject) weapon;
@@ -42,8 +44,9 @@ public class WeaponHandler {
 
     public Weapon generateWeapon(String name, GameState game){
         JSONObject weapon = weapons.get(name);
-        if(weapon.get("type")=="ranged_standard")
-            return new RangedStandardWeapon(0,0,(String)weapon.get("name"),((Number)weapon.get("firerate")).intValue(), game, projectiles.get(name));
+        System.out.println((String)weapon.get("type"));
+        if(((String)weapon.get("type")).equals("ranged standard"))
+            return new RangedStandardWeapon(0,0,(String)weapon.get("name"),((Number)weapon.get("firerate")).intValue(), game, projectiles.get((String)weapon.get("projectile")));
         else
             return null;
     }
